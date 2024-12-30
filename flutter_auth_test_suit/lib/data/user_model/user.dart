a1cc12f6a4a9a49279e7b85a940825ce32fc0c5a
hungjen9699 <hungjen9699@gmail.com>
2024-12-30 20:19:46 +0700

    feat:  add user model

--- flutter_auth_test_suit/lib/data/user_model/user.dart
+++ flutter_auth_test_suit/lib/data/user_model/user.dart
@@ -1,20 +1,43 @@
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
-  final String id;
-  final String email;
-  final String name;
+  final int id;
+  final String username;
+  final String? email;
+  final String? firstName;
+  final String? lastName;
+  final String? gender;
+  final String? image;
 
-  const User({required this.id, required this.email, required this.name});
+  const User(
+      {required this.id,
+      required this.username,
+      this.email,
+      this.firstName,
+      this.lastName,
+      this.gender,
+      this.image});
 
   @override
-  List<Object?> get props => [id, email, name];
+  List<Object?> get props => [
+        id,
+        email,
+        email,
+        firstName,
+        lastName,
+        gender,
+        image,
+      ];
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      id: json['id'] as String,
-      email: json['email'] as String,
-      name: json['name'] as String,
+      id: json['id'] as int,
+      username: json['username'] as String,
+      email: json['email'] as String?,
+      firstName: json['firstName'] as String?,
+      lastName: json['lastName'] as String?,
+      gender: json['gender'] as String?,
+      image: json['image'] as String?,
     );
   }
 
@@ -22,7 +45,11 @@ class User extends Equatable {
     return {
       'id': id,
       'email': email,
-      'name': name,
+      'username': username,
+      'firstName': firstName,
+      'lastName': lastName,
+      'gender': gender,
+      'image': image,
     };
   }
 }
